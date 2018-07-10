95fe799de831a00858fcf2535851fdc88b8d8291
Naveen Tak <naveentak@gmail.com>
2018-07-10 08:25:36 +0200

    Initial commit

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ function hello(name)
    
     
 }
+
+let response = 
+{
+    greet(name) {
+    var greeting = "Hello";
+    return greeting + ' ' + name;
+
+    }
+    
+}
 console.log('hello world');
 console.log(hello("Node"));
+console.log(response.greet("Naveen"));
 //console.log(hello.greeting);
 
 {
-    var num1 = 45;
+    let num1 = 45;
     {
         var num2 = 43;
+        console.log(num1);
     }
 }
-console.log(num1);
+
 console.log(num2);
---
+++ test.js
@@ -0,0 +1,33 @@
+function hello(name)
+{
+    var greeting = "Hello";
+    return greeting + ' ' + name;
+
+   
+    
+}
+
+let response = 
+{
+    greet(name) {
+    var greeting = "Hello";
+    return greeting + ' ' + name;
+
+    }
+    
+}
+console.log('hello world');
+console.log(hello("Node"));
+console.log(response.greet("Naveen"));
+//console.log(hello.greeting);
+
+{
+    let num1 = 45;
+    {
+        var num2 = 43;
+        console.log(num1);
+    }
+}
+
+console.log(num2);
+
